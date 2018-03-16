09f9688ec1676e3bac8a3e5928268dc95781a74b
josephdlawson21 <josephdlawson21@gmail.com>
2018-03-16 09:54:49 -0400

    added cors, fetch park info"

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -8,6 +8,11 @@ class ParksController < ApplicationController
     park = Park.find_by(parkCode: params['parkCode'])
 
     render json: park
-
   end
+
+  def frontPage
+    articles = RestClient.get "https://developer.nps.gov/api/v1/articles?limit=7&api_key=R7qhvnG45YlTSKHI5IrQsqDsMjR0VRaqZPTmVZky"
+    render json: articles
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/parks/frontPageArticles', to: 'parks#frontPage'
   get '/parks', to: 'parks#index'
   get '/parks/:parkCode', to: 'parks#show'
+
 
 end
